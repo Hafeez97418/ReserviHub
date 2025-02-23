c35ab48be2c3b76987c78bd093a582375f7b9a95
Hafeez <muhammed.hafeez.nayazi@gmail.com>
2025-02-23 11:26:55 +0530

    model assosiations done

---
+++ src/models/database.ts
@@ -0,0 +1,46 @@
+import Appointment from "./Appointment.js";
+import AppointmentInterval from "./AppointmentInterval.js";
+import Business from "./Business.js";
+import BusinessAnalytics from "./BusinessAnalytics.js";
+import AiChatbotPredictions from "./ChatBotPredictions.js";
+import Payment from "./Payments.js";
+import Review from "./Reviews.js";
+import User from "./User.js";
+
+User.hasMany(Business, { foreignKey: "managerId" });
+Business.belongsTo(User, { foreignKey: "managerId" });
+
+Business.hasMany(AppointmentInterval, { foreignKey: "businessId" });
+AppointmentInterval.belongsTo(Business, { foreignKey: "businessId" });
+
+User.hasMany(Appointment, { foreignKey: "userId" });
+Appointment.belongsTo(User, { foreignKey: "userId" });
+
+Business.hasMany(Appointment, { foreignKey: "businessId" });
+Appointment.belongsTo(Business, { foreignKey: "businessId" });
+
+AppointmentInterval.hasMany(Appointment, { foreignKey: "intervalId" });
+Appointment.belongsTo(AppointmentInterval, { foreignKey: "intervalId" });
+
+User.hasMany(Payment, { foreignKey: "userId" });
+Appointment.hasOne(Payment, { foreignKey: "appointmentId" });
+
+User.hasMany(Review, { foreignKey: "userId" });
+Business.hasMany(Review, { foreignKey: "businessId" });
+
+Business.hasOne(BusinessAnalytics, { foreignKey: "businessId" });
+BusinessAnalytics.belongsTo(Business, { foreignKey: "businessId" });
+
+Business.hasMany(AiChatbotPredictions, { foreignKey: "businessId" });
+AiChatbotPredictions.belongsTo(Business, { foreignKey: "businessId" });
+
+export {
+  User,
+  Business,
+  BusinessAnalytics,
+  AiChatbotPredictions,
+  Appointment,
+  AppointmentInterval,
+  Review,
+  Payment,
+};
