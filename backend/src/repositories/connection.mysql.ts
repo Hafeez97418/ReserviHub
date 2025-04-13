9bb1e54a6f018b989b120d203e37359cd2c7f542
Hafeez <muhammed.hafeez.nayazi@gmail.com>
2025-04-13 22:54:30 +0530

    added database port for hosting

--- backend/src/repositories/connection.mysql.ts
+++ backend/src/repositories/connection.mysql.ts
@@ -5,23 +5,29 @@ const db = {
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
 };
-const { username, name, password, host } = db;
+const { username, name, password, host, port } = db;
 
-
-if (!username || !name || !password || !host) {
+if (!username || !name || !password || !host || isNaN(port)) {
   throw new Error("some database credentials are undefined");
 }
 
 export const sequelize = new Sequelize(name, username, password, {
   host: host,
   dialect: "mysql",
+  port: port,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: true,
+    },
+  },
 });
 
 export const authenticateDB = async () => {
   try {
     await sequelize.sync();
-    console.log("database successfully connected");
+    console.log("database successfully connected on port ", port);
   } catch (error) {
     throw new Error("Unable to connect to the database: " + `${error}`);
   }
